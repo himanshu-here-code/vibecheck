efa53e14384a5759c3ca88c39c0e6a5646acb0c2
Himanshu <himanshu.dabla34@gmail.com>
2026-09-21 22:54:14 +0530

    Minor fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { GithubIcon } from './Icons';
 
 export function Header() {
   function handleLogoClick() {
-    // Tell the home page to reset its scan state
     if (typeof window !== 'undefined') {
       window.dispatchEvent(new Event('vibecheck:reset'));
     }
